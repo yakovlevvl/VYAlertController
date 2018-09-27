8e515a4d68919bde0b65feaddcf17bab33eea43e
Vladyslav Yakovlev <yakovlev15@icloud.com>
2018-09-27 19:24:32 +0300

    Initial Commit

---
+++ VYAlertControllerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VYAlertControllerExample
+//
+//  Created by Vladyslav Yakovlev on 9/27/18.
+//  Copyright © 2018 Vladyslav Yakovlev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
